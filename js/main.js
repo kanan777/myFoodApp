24854d62384864b8b2341c577c800f8473d2f8a5
kanan777 <kelekberov42@gmail.com>
2023-05-14 00:09:39 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: -67,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});
